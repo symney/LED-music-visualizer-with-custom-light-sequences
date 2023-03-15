b03f65667aaa72a59fc24c22e84d29deeab3f81c
symney <53664279+symney@users.noreply.github.com>
2023-03-15 11:10:12 -0400

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-redis-server;
-echo -e "Create an account and paste your ngrok authtoken here after you paste hit enter then control d: \n" &&
-ngrok config add-authtoken `cat  `;
-cd python
-openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365;
-sudo python3 server.py &
-ngrok http https://localhost:5000
