75c3e9eef4cc7afa89e78f77f6cef2f1b1a7dc03
symney <53664279+symney@users.noreply.github.com>
2023-03-17 04:27:30 -0400

    Update start.sh
    updated start.sh to check for file existence of authtoken.txt and the ssl certificates

--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
 redis-server;
-echo -e "Create an account and paste your ngrok authtoken here after you paste hit enter then control d: \n" &&
+
+
+if test -e "./authtoken.txt"; then
+echo "found the ngrok auth token. Using that."
 ngrok config add-authtoken `cat authtoken.txt`;
+
+else
+echo "Did not find the ngrok auth token";
+echo -e "Create an account and paste your ngrok authtoken here after you paste hit enter then control d: \n" &&
+ngrok config add-authtoken `cat `;
+fi
+
+
 cd python
+if test -e "./key.pem" && test -e "./cert.pem"; then
+echo "found the ssl certficate files"
+else
+echo "did not fid the ssl certificate files making them now";
 openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365;
+fi
 sudo python3 server.py &
 ngrok http https://localhost:5000
